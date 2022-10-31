0e2b2aff9133cea6162690d871fd2f764f020244
Philip Huang <philiphuang2@gmail.com>
2022-10-31 12:36:49 -0700

    added seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO movies(movie_name)
+VALUES ("Holes"),
+("12 Angry Men"),
+("Terminator"),
+("Deadpool");
+
+
+INSERT INTO reviews(movie_id, review)
+VALUES
+(1, "This is about holes.  I love it"),
+(1, "This has 1 big hole in it"),
+(3, " asdasdkqwpemksxclas"),
+(4, "Actually Garbage"),
+(2, "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit");
