835ce92b4c54d323129a7e2e79fe13be925da0bc
Philip Huang <philiphuang2@gmail.com>
2022-10-31 12:29:13 -0700

    created schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS movie_db;
+CREATE DATABASE movie_db;
+
+use movie_db;
+
+CREATE TABLE movies(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    movie_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE reviews(  
+    id INT NOT NULL  AUTO_INCREMENT PRIMARY KEY,
+    movie_id INT,
+    review TEXT,
+    FOREIGN KEY(movie_id) REFERENCES movies(id) ON DELETE SET NULL
+);
+
